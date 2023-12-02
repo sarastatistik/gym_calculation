80aa73216559ecc6376e5b752ce2818ede23ab60
Sara Johansson <sarajohanssoon@proton.me>
2023-12-02 10:22:25 +0100

    Gym stuff

--- gym_calculation/app.py
+++ gym_calculation/app.py
@@ -1,34 +1,107 @@
 from pathlib import Path
 
-from shiny import App, render, ui
+from shiny import App, render, ui, reactive
 import shinyswatch
 
-from gym_schedule import MonthlySchedule
+import pandas as pd
+
+from gym_schedule import MonthlySchedule, WeightCalc
 
 
-gym = MonthlySchedule()
+gym = MonthlySchedule("gym_calculation/exercises_acc.json",
+                       "gym_calculation/exercises_prehab.json",
+                       weight_calc=WeightCalc(70, 47.5, 102.5))
 
-phases = {"Accumulation": ("Week 1: Accumulation", gym.week_one),
-"Intensification": ("Week 2: Intensification", gym.week_two),
-"Peaking": ("Week 3: Peaking", gym.week_three),
-"Deloading": ("Week 4: Deloading", gym.week_four)}
+
+
+
+css_path = "style.css"
+
+
+
+phases = {
+    "Week 1": (f"Week 1: Accumulation ({gym.prc[0]})", gym.main_week_one, gym.acc_week_one),
+    "Week 2": (f"Week 2: Intensification ({gym.prc[1]})", gym.main_week_two, gym.acc_week_two),
+    "Week 3": (f"Week 3: Peaking ({gym.prc[2]})", gym.main_week_three, gym.acc_week_three),
+    "Week 4": (f"Week 4: Deload ({gym.prc[3]})", gym.sessions_week_four)
+}
+
+
+headers = {
+    "Week 1": gym.headers_main,
+    "Week 2": gym.headers_main,
+    "Week 3": gym.headers_main,
+    "Week 4": gym.headers_deload
+}
 
 
 lifts = {0: "squats", 1: "bench", 2: "deadlift"}
 
 
+week123 = ui.row(
+            ui.column(4,
+                      ui.h5(ui.output_text("session_header1")),
+                      ui.markdown("*15 min warmup*"),
+                      ui.h6("Main lift"),
+                      ui.output_table("main1"),
+                      ui.h6("Accessory lifts"),
+                      ui.output_table("acc1"),
+                      ui.h6("Prehab exercises"),
+                      ui.output_table("pre1")),
+            ui.column(4,
+                      ui.h5(ui.output_text("session_header2")), 
+                      ui.markdown("*15 min warmup*"),
+                      ui.h6("Main lift"),
+                      ui.output_table("main2"),
+                      ui.h6("Accessory lifts"),
+                      ui.output_table("acc2"),
+                      ui.h6("Prehab exercises"),
+                      ui.output_table("pre2")),
+            ui.column(4,
+                      ui.h5(ui.output_text("session_header3")),
+                      ui.markdown("*15 min warmup*"),
+                      ui.h6("Main lift"),
+                      ui.output_table("main3"),
+                      ui.h6("Accessory lifts"),
+                      ui.output_table("acc3"),
+                      ui.h6("Prehab exercises"),
+                      ui.output_table("pre3")),
+            align="center"
+        )
+
+
+
+
+week4 = ui.row(
+            ui.column(4,
+                      ui.h5(ui.output_text("session_header1")),
+                      ui.markdown("*15 min warmup*"),
+                      ui.output_table("main1")),
+            ui.column(4,
+                      ui.h5(ui.output_text("session_header2")),
+                      ui.markdown("*15 min warmup*"), 
+                      ui.output_table("main2")),
+            ui.column(4,
+                      ui.h5(ui.output_text("session_header3")),
+                      ui.markdown("*15 min warmup*"),
+                      ui.output_table("main3")),
+            align="center"
+        )
+
+
 app_ui = ui.page_fluid(
     shinyswatch.theme.minty(),
+    ui.include_css(css_path),
     ui.markdown("<br>"),
     ui.row(
         ui.column(
             6,
-            ui.panel_title("Sara's Workout Schedule"),
+            ui.h2("Sara's Workout Schedule"),
         ),
         ui.column(
             6,
             ui.popover(
-                ui.input_action_button("btn", "Info", class_="btn-outline-primary"),
+                ui.input_action_button("btn", "Info", class_="btn-primary"),
                 "I'm not a professional trainer and this is not a recommendation. "
                 "This is just for my own personal use.",
                 id="btn_popover",
@@ -36,50 +109,48 @@ app_ui = ui.page_fluid(
             align="right")),
     ui.layout_sidebar(
         ui.sidebar(
+            ui.h5("Select the current phase:"),
             ui.input_select("gym_phase",
-                            "Select the current phase",
-                            list(phases.keys())),
-            ui.hr(),
-            width="300px"
+                            "",
+                            list(phases.keys()),
+                            selected="Week 1"),
+            ui.input_action_button("btn_acc", "Change accessory lifts", class_="btn-primary"),
+            ui.input_action_button("btn_pre", "Change prehab exercises", class_="btn-primary"),             
+            ui.markdown("<br>"),
+            ui.row(
+                "detta filter gör inget nu - ändrar sen vikt i schema",
+                ui.h5("Fill out 1RM for each lift:"),
+                ui.column(4, ui.input_numeric("rm1_s", "Squats", 70, min=20, step=2.5)),
+                ui.column(4, ui.input_numeric("rm1_b", "Bench", 47.5, min=20, step=2.5)),
+                ui.column(4, ui.input_numeric("rm1_d", "Deadlift", 102.5, min=20, step=2.5))
+            ),
+            width="400px"
         ),
+        {"style": "background-color: #fff"},
         ui.row(
             ui.h3(ui.output_text("phase")),
             align="center"
         ),
-        ui.row(
-            ui.column(4, ui.h5(ui.output_text("session_header1")), ui.output_table("session1")),
-            ui.column(4, ui.h5(ui.output_text("session_header2")), ui.output_table("session2")),
-            ui.column(4, ui.h5(ui.output_text("session_header3")), ui.output_table("session3")),
-            align="center"
-        )
+        ui.output_ui("ui_main_part")
     )
 )
 
 
 def server(input, output, session):
-    @output
-    @render.text
-    def session_header1():
-        if input.gym_phase() == "Deloading":
-            return "Full body session"
-        else:
-            return "Squats session"
+    acc_squats = reactive.Value(gym.accessory.squats[0])
+    acc_bench = reactive.Value(gym.accessory.bench[0])
+    acc_deadlift = reactive.Value(gym.accessory.deadlift[0])
+    pre_squats = reactive.Value(gym.prehab.squats[:2])
+    pre_bench = reactive.Value(gym.prehab.bench[:2])
+    pre_deadlift = reactive.Value(gym.prehab.deadlift[:2])
 
     @output
-    @render.text
-    def session_header2():
-        if input.gym_phase() == "Deloading":
-            return "Upper body session"
+    @render.ui
+    def ui_main_part():
+        if input.gym_phase() == "Week 4":
+            return week4
         else:
-            return "Bench session"
-
-    @output
-    @render.text
-    def session_header3():
-        if input.gym_phase() == "Deloading":
-            return "Lower body session"
-        else:
-            return "Deadlift session"
+            return week123
 
     @output
     @render.text
@@ -87,12 +158,24 @@ def server(input, output, session):
         return phases[input.gym_phase()][0]
 
     @output
+    @render.text
+    def session_header1():
+        return headers[input.gym_phase()][0]
+
+    @output
+    @render.text
+    def session_header2():
+        return headers[input.gym_phase()][1]
+
+    @output
+    @render.text
+    def session_header3():
+        return headers[input.gym_phase()][2]
+
+    @output
     @render.table
-    def session1():
-        if input.gym_phase() == "Deloading":
-            df = phases[input.gym_phase()][1].full_body_session()
-        else:
-            df = phases[input.gym_phase()][1].generate_sessions()[0]
+    def main1():
+        df = phases[input.gym_phase()][1]()[0]
         return (df.style
                 .set_table_attributes('class="dataframe table shiny-table w-auto"')
                 .hide(axis="index")
@@ -100,11 +183,8 @@ def server(input, output, session):
 
     @output
     @render.table
-    def session2():
-        if input.gym_phase() == "Deloading":
-            df = phases[input.gym_phase()][1].upper_body_session()
-        else:
-            df = phases[input.gym_phase()][1].generate_sessions()[1]
+    def main2():
+        df = phases[input.gym_phase()][1]()[1]
         return (df.style
                 .set_table_attributes('class="dataframe table shiny-table w-auto"')
                 .hide(axis="index")
@@ -112,15 +192,137 @@ def server(input, output, session):
 
     @output
     @render.table
-    def session3():
-        if input.gym_phase() == "Deloading":
-            df = phases[input.gym_phase()][1].lower_body_session()
-        else:
-            df = phases[input.gym_phase()][1].generate_sessions()[2]
+    def main3():
+        df = phases[input.gym_phase()][1]()[2]
         return (df.style
                 .set_table_attributes('class="dataframe table shiny-table w-auto"')
                 .hide(axis="index")
                 .hide(axis="columns"))
+    @output
+    @render.table
+    def acc1():
+        if input.gym_phase() != "Week 4":
+            df = phases[input.gym_phase()][2]()[0]
+            df.loc[df["lift"] == "bench", "lift"] = acc_bench()
+            df.loc[df["lift"] == "deadlift", "lift"] = acc_deadlift()
+            return (df.style
+                    .set_table_attributes('class="dataframe table shiny-table w-auto"')
+                    .hide(axis="index")
+                    .hide(axis="columns"))
+
+    @output
+    @render.table
+    def acc2():
+        if input.gym_phase() != "Week 4":
+            df = phases[input.gym_phase()][2]()[1]
+            df.loc[df["lift"] == "squats", "lift"] = acc_squats()
+            df.loc[df["lift"] == "deadlift", "lift"] = acc_deadlift()
+            return (df.style
+                    .set_table_attributes('class="dataframe table shiny-table w-auto"')
+                    .hide(axis="index")
+                    .hide(axis="columns"))
+
+    @output
+    @render.table
+    def acc3():
+        if input.gym_phase() != "Week 4":
+            df = phases[input.gym_phase()][2]()[2]
+            df.loc[df["lift"] == "bench", "lift"] = acc_bench()
+            df.loc[df["lift"] == "squats", "lift"] = acc_squats()
+            return (df.style
+                    .set_table_attributes('class="dataframe table shiny-table w-auto"')
+                    .hide(axis="index")
+                    .hide(axis="columns"))
+        
+    @output
+    @render.table
+    def pre1():
+        df = pd.DataFrame(pre_squats())
+        return (df.style
+                .set_table_attributes('class="dataframe table shiny-table w-auto"')
+                .hide(axis="index")
+                .hide(axis="columns"))
+
+    @output
+    @render.table
+    def pre2():
+        df = pd.DataFrame(pre_bench())
+        return (df.style
+                .set_table_attributes('class="dataframe table shiny-table w-auto"')
+                .hide(axis="index")
+                .hide(axis="columns"))
+
+    @output
+    @render.table
+    def pre3():
+        df = pd.DataFrame(pre_deadlift())
+        return (df.style
+                .set_table_attributes('class="dataframe table shiny-table w-auto"')
+                .hide(axis="index")
+                .hide(axis="columns"))
+
+    @reactive.Effect
+    @reactive.event(input.btn_acc)
+    def _():
+        m = ui.modal(
+            ui.input_select("acc_s", "Squats", gym.accessory.squats, selected=acc_squats()),
+            ui.input_select("acc_b", "Bench", gym.accessory.bench, selected=acc_bench()),
+            ui.input_select("acc_d", "Deadlift", gym.accessory.deadlift, selected=acc_deadlift()),
+            title="Accessory Lifts",
+            easy_close=True,
+            size="m"
+        )
+        ui.modal_show(m)
+
+    @reactive.Effect
+    @reactive.event(input.btn_pre)
+    def _():
+        m = ui.modal(
+            ui.input_select("pre_s", "Squats", gym.prehab.squats, selected=pre_squats(), selectize=True, multiple=True),
+            ui.input_select("pre_b", "Bench", gym.prehab.bench, selected=pre_bench(), selectize=True, multiple=True),
+            ui.input_select("pre_d", "Deadlift", gym.prehab.deadlift, selected=pre_deadlift(), selectize=True, multiple=True),
+            title="Prehab Exercises",
+            easy_close=True,
+            size="m"
+        )
+        ui.modal_show(m)
+
+    @reactive.Effect
+    @reactive.event(input.acc_s)
+    def _():
+        new = input.acc_s()
+        acc_squats.set(new)
+
+
+    @reactive.Effect
+    @reactive.event(input.acc_b)
+    def _():
+        new = input.acc_b()
+        acc_bench.set(new)
+
+    @reactive.Effect
+    @reactive.event(input.acc_d)
+    def _():
+        new = input.acc_d()
+        acc_deadlift.set(new)
+
+    @reactive.Effect
+    @reactive.event(input.pre_s)
+    def _():
+        new = input.pre_s()
+        pre_squats.set(new)
+
+    @reactive.Effect
+    @reactive.event(input.pre_b)
+    def _():
+        new = input.pre_b()
+        pre_bench.set(new)
+
+    @reactive.Effect
+    @reactive.event(input.pre_d)
+    def _():
+        new = input.pre_d()
+        pre_deadlift.set(new)
 
 
 app = App(app_ui, server)
--- gym_calculation/gym_schedule.py
+++ gym_calculation/gym_schedule.py
@@ -1,32 +1,115 @@
+from dataclasses import dataclass
+import json
 from typing import Dict, Optional, List, Union
 
 import numpy as np
 import pandas as pd
 
 
+
+
+class WeightCalc:
+    def __init__(self,
+                 squats: Union[int, float],
+                 bench: Union[int, float],
+                 deadlift: Union[int, float],
+                 warmup_sets: int = 5) -> None:
+        
+        self.weight_increase = 2.5
+        self.warmup_sets = warmup_sets
+
+        self.prc = {
+            "squats": [0.7, 0.8, 0.9],
+            "bench": [0.85, 0.9, 0.975],
+            "deadlift": [0.8, 0.87, 0.95]
+        }
+
+        self.working_prc = [0.8, 0.85, 0.9]
+
+        self.weights = {
+            "squats": squats,
+            "bench": bench,
+            "deadlift": deadlift
+        }
+        self.starting_weights = {
+            "squats": 20,
+            "bench": 20,
+            "deadlift": 60
+        }
+
+        self._setup_weights()
+    
+    def _get_warmup_weights(self, lift: str, x: Union[float, int]):
+        if lift != "bench":
+            y = np.linspace(self.starting_weights[lift], x, self.warmup_sets)
+            y = [self._round_set(i) for i in y]
+            y = [round(i, -1) if i < 40 else i for i in y]
+        else:
+            y = np.linspace(self.starting_weights[lift] + 10, x, self.warmup_sets - 1)
+            y = [self.starting_weights[lift]] + [self._round_set(i) for i in y]
+        return y
+
+    def _setup_weights(self):
+        self.one_rep = {
+            "squats": [self._round_set(x * self.weights["squats"]) for x in self.prc["squats"]],
+            "bench": [self._round_set(x * self.weights["bench"]) for x in self.prc["bench"]],
+            "deadlift": [self._round_set(x * self.weights["deadlift"]) for x in self.prc["deadlift"]]
+        }
+        self.working_weights = {
+            "squats": [self._round_set(x * y) for x, y in zip(self.one_rep["squats"], self.working_prc)],
+            "bench": [self._round_set(x * y) for x, y in zip(self.one_rep["bench"], self.working_prc)],
+            "deadlift": [self._round_set(x * y) for x, y in zip(self.one_rep["deadlift"], self.working_prc)]
+        }
+
+        self.warmup_weights = {}
+        for k in self.one_rep.keys():
+            self.warmup_weights[k] = [self._get_warmup_weights(k, x) for x in self.one_rep[k]]
+        
+        self.deload_weights = {
+            "squats": [20] + [self._round_set(0.6 * self.weights["squats"])]*2,
+            "bench": [20] + [self._round_set(0.6 * self.weights["bench"])]*2,
+            "deadlift": [40] + [self._round_set(0.6 * self.weights["deadlift"])]*2
+        }
+
+    def _round_set(self, x: Union[int, float]):
+        # Based on 2.5
+        y = round(x, -1) - 10
+        y = [y + self.weight_increase*i for i in range(10)]
+        z = [abs(i - x) for i in y]
+        z = np.where(pd.Series(z) == min(z))[0][0]        
+        return y[z]
+
+    def update_squats(self, x: Union[float, int]):
+        self.weights["squats"] = x
+        self._setup_weights()
+
+    def update_bench(self, x: Union[float, int]):
+        self.weights["bench"] = x
+        self._setup_weights()
+
+    def update_deadlift(self, x: Union[float, int]):
+        self.weights["deadlift"] = x
+        self._setup_weights()
+
+    def get_weights(self, lift: str):
+        w = []
+        for x, y in zip(self.warmup_weights[lift], self.working_weights[lift]):
+            w.append(x + [y])
+        return w
+    
+
+
 class GymSchedule:
 
     def __init__(self,
                  main_set_reps: Optional[int] = None,
-                 accessory_set_reps: Optional[int] = None,
-                 prehab_set_reps: int = 6,
-                 accessory_lifts: Optional[Dict[str, str]] = None,
-                 prehab_lifts: Optional[Dict[str, str]] = None) -> None:
+                 accessory_set_reps: Optional[int] = None) -> None:
 
         self.main_lifts = ["squats", "bench", "deadlift"]
 
-        if accessory_lifts is None:
-            accessory_lifts = {k: f"acc-{k}" for k in self.main_lifts}
-
-        if prehab_lifts is None:
-            prehab_lifts = {k: f"prehab-{k}" for k in self.main_lifts}
-
-        self.accessory_lifts = accessory_lifts
-        self.prehab_lifts = prehab_lifts
-
         if main_set_reps is not None:
             self.sets_main = GymSchedule.generate_sets(
-                warmup_sets=3,
+                warmup_sets=4,
                 working_sets=3,
                 ending_reps=1,
                 max_reps=main_set_reps)
@@ -35,20 +118,12 @@ class GymSchedule:
 
         if accessory_set_reps is not None:
             self.sets_accessory = GymSchedule.generate_sets(
-                warmup_sets=2,
+                warmup_sets=3,
                 working_sets=3,
                 ending_reps=2,
                 max_reps=accessory_set_reps)
         else:
             self.sets_accessory = None
-
-        self.sets_prehab = GymSchedule.generate_sets(
-            warmup_sets=0,
-            working_sets=3,
-            ending_reps=0,
-            max_reps=prehab_set_reps,
-            starting_with_bar=False
-        )
 
     @staticmethod
     def generate_sets(warmup_sets: int,
@@ -67,25 +142,23 @@ class GymSchedule:
              return sets[0]
         return sets
     
-    def _generate_session(self, main_lift: str) -> pd.DataFrame:
+    def generate_main(self, main_lift: str) -> pd.DataFrame:
         sets = [(main_lift, i) for i in self.sets_main]
-        acc = [v for k, v in self.accessory_lifts.items() if k != main_lift]
+        return pd.DataFrame(sets, columns=["lift", "setup"])
+
+    def generate_acc(self, main_lift: str):
+        acc = [k for k in self.main_lifts if k != main_lift]
+        sets = []
         for i in acc:
             for j in self.sets_accessory:
-                sets.append((i, j))            
-        sets.append((self.prehab_lifts[main_lift], self.sets_prehab))
-        sets = pd.DataFrame(sets, columns=["lift", "setup"])
-        return sets
-    
-    def generate_sessions(self) -> List[pd.DataFrame]:
-        return [self._generate_session(m) for m in self.main_lifts]
+                sets.append((i, j))
+        return pd.DataFrame(sets, columns=["lift", "setup"])
 
 
 class DeloadSchedule(GymSchedule):
 
-    def __init__(self,
-                 prehab_set_reps: int = 8) -> None:
-        super().__init__(prehab_set_reps=prehab_set_reps)
+    def __init__(self) -> None:
+        super().__init__()
 
     def full_body_session(self):
         # 2 sets of 5 reps at 50-60% of 1RM
@@ -98,16 +171,23 @@ class DeloadSchedule(GymSchedule):
         for m in self.main_lifts:
             for s in sets:
                 session.append((m, s))
-        for p in self.prehab_lifts.values():
-            session.append((p, self.sets_prehab))
+        prehab = [
+            "Hip exercises (variations)",
+            "Scapular Retraction",
+            "Band Pull-Aparts",
+        ]
+        for p in prehab:
+            session.append((p, ""))
+
         return pd.DataFrame(session, columns=["lift", "setup"])
     
     def _prehab_session(self, exercises: List[str]) -> pd.DataFrame:
-        session = [(e, self.sets_prehab) for e in exercises]
-        return pd.DataFrame(session, columns=["lift", "setup"])
+        return pd.DataFrame(exercises, columns=["lift"])
 
     def upper_body_session(self):
         exercises = [
+            "Prone YTI Raises",
+            "Reverse Prayer Hands",
             "Jefferson Curls",
             "Side Lateral Shoulder Raises",
             "Triceps Pushdowns",
@@ -122,9 +202,11 @@ class DeloadSchedule(GymSchedule):
         exercises = [
             "Deadbugs",
             "Fire Hydrant",
-            "Bridges",
-            "Single Leg Bridge",
+            "Glute Bridges (variations)",
+            "Clamshells",
             "Bird Dog",
+            "Kettlebell Swings",
+            "Calf Raises",
             "Kickbacks",
             "Leg Extension",
         ]
@@ -138,18 +220,103 @@ class DeloadSchedule(GymSchedule):
 
 
 
+
+
+class ExerciseBucket:
+
+    def __init__(self,
+                 squats: List[str], 
+                 bench: List[str],
+                 deadlift: List[str]) -> None:
+        self.squats = squats
+        self.bench = bench
+        self.deadlift = deadlift
+        self.all = squats + bench + deadlift
+
+
 class MonthlySchedule:
 
-    def __init__(self) -> None:
-        self.week_one = GymSchedule(main_set_reps=8, accessory_set_reps=6)
-        self.week_two = GymSchedule(main_set_reps=5, accessory_set_reps=4)
-        self.week_three = GymSchedule(main_set_reps=2, accessory_set_reps=1)
-        self.week_four = DeloadSchedule(prehab_set_reps=8)
+    def __init__(self, accessory: str, prehab: str, weight_calc: WeightCalc) -> None:
+        self.weight_calc = weight_calc
+        
+        self.weight_calc.get_weights("bench")
+        self.weight_calc.get_weights("deadlift")
 
 
-self = MonthlySchedule()
+        self.prc = ["70%", "80%", "90%", "55%"]
+        self.headers_main = ["Squats session", "Bench session", "Deadlift session"]
+        self.headers_deload = ["Full body session", "Upper body session", "Lower Body Session"]
+
+        with open(accessory, "r", encoding="utf-8") as f:
+            self.accessory = ExerciseBucket(**json.load(f))
+
+
+        with open(prehab, "r", encoding="utf-8") as f:
+            self.prehab = ExerciseBucket(**json.load(f))
+
+        self.accumulation = GymSchedule(main_set_reps=8, accessory_set_reps=6)
+        self.intensification = GymSchedule(main_set_reps=5, accessory_set_reps=4)
+        self.peaking = GymSchedule(main_set_reps=2, accessory_set_reps=1)
+        self.deloading = DeloadSchedule()
+
+    @staticmethod
+    def rename_main_lifts(df: pd.DataFrame):
+        df.loc[df["lift"] == "squats", "lift"] = "Squats"
+        df.loc[df["lift"] == "bench", "lift"] = "Bench Press"
+        df.loc[df["lift"] == "deadlift", "lift"] = "Deadlift"
+        return df
+   
+    def main_week_one(self):
+        d = [self.accumulation.generate_main(m) for m in self.accumulation.main_lifts]
+        w = [self.weight_calc.get_weights(lift)[0] for lift in ["squats", "bench", "deadlift"]]
+        for x, y in zip(d, w):
+            x.insert(2, "weight", y)
+            x["weight"] = [str(round(i,1)) if str(i)[-1] != "0" else str(round(i)) for i in x["weight"]]
+            x["weight"] = [i + "kg" for i in x["weight"]]
+        return [MonthlySchedule.rename_main_lifts(df) for df in d]
+
+    def main_week_two(self):
+        d = [self.intensification.generate_main(m) for m in self.intensification.main_lifts]
+        w = [self.weight_calc.get_weights(lift)[1] for lift in ["squats", "bench", "deadlift"]]
+        for x, y in zip(d, w):
+            x.insert(2, "weight", y)
+            x["weight"] = [str(round(i,1)) if str(i)[-1] != "0" else str(round(i)) for i in x["weight"]]
+            x["weight"] = [i + "kg" for i in x["weight"]]
+        return [MonthlySchedule.rename_main_lifts(df) for df in d]
+
+    def main_week_three(self):
+        d = [self.peaking.generate_main(m) for m in self.peaking.main_lifts]
+        w = [self.weight_calc.get_weights(lift)[2] for lift in ["squats", "bench", "deadlift"]]
+        for x, y in zip(d, w):
+            x.insert(2, "weight", y)
+            x["weight"] = [str(round(i,1)) if str(i)[-1] != "0" else str(round(i)) for i in x["weight"]]
+            x["weight"] = [i + "kg" for i in x["weight"]]
+        return [MonthlySchedule.rename_main_lifts(df) for df in d]
+    
+    def acc_week_one(self):
+        d = [self.accumulation.generate_acc(m) for m in self.accumulation.main_lifts]
+        return d
+
+    def acc_week_two(self):
+        d = [self.intensification.generate_acc(m) for m in self.intensification.main_lifts]
+        return d
+
+    def acc_week_three(self):
+        d = [self.peaking.generate_acc(m) for m in self.peaking.main_lifts]
+        return d
+
+    def sessions_week_four(self):
+        d = [self.deloading.full_body_session(), self.deloading.upper_body_session(), self.deloading.lower_body_session()]
+        return [MonthlySchedule.rename_main_lifts(df) for df in d]
+
+
+gym = MonthlySchedule("gym_calculation/exercises_acc.json",
+                       "gym_calculation/exercises_prehab.json",
+                       weight_calc=WeightCalc(70, 47.5, 102.5))
 
 
 
 
+
+self = gym
 
